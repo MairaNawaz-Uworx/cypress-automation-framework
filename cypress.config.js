3c8c6e350e2c29bb4e2f7a37233db34456e6dcd2
MairaNawaz-Uworx <maira.nawaz@uworx.co.uk>
2024-02-06 10:51:10 +0500

    Updated

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
-  projectId: "4qkjxa",
+  projectId: "6m2ry8",
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
